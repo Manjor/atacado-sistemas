804376c9a9165f258055c45d27225df7a8a4a857
Manoel Tavares <manoeltavares.jr@gmail.com>
2019-11-28 17:23:19 -0300

    Primeiro Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex md12 mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bem Vindo ao Atacado
+        </h1>
+        <p class="subheading font-weight-regular">
+          Aplicação criada apenas para demonstração na disciplina de Sistemas Distribuídos,
+          <br>Direitos reservados a Manoel Tavares
+        </p>
+      </v-flex>
+
+      <v-flex md12 mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Estatisticas
+        </h1>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Nome</th>
+                <th class="text-left">Email</th>
+                <th class="text-right">Pontos</th>
+                <th class="text-right">Compras</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item,idx) in users" :key="idx">
+                <td class="text-left">{{ item.name }}</td>
+                <td class="text-left">{{ item.email }}</td>
+                <td class="text-right">{{ item.pontos || 0}}</td>
+                <td class="text-right">{{ item.compras || 0}}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-flex>
+
+      
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    userCount: 0,
+    users: [],
+  }),
+  methods: ({
+    getStats(){
+      axios.get('http://localhost:3000/stats')
+        .then(res => {
+          this.userCount = res.data.users,
+          this.users = res.data.lastInto
+        })
+    }
+  }),
+  mounted(){
+    this.getStats();
+  }
+};
+</script>
